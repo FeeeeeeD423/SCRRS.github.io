650415be1027905d1d0aa24e072cd2870bd5604a
FeeeeeeD423 <150744506+FeeeeeeD423@users.noreply.github.com>
2023-11-17 17:12:52 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById('registroConsulta').addEventListener('click', function() {
+    document.querySelector('.menu-principal').style.display = 'none';
+    document.querySelector('.registro-consulta').style.display = 'block';
+});
+
+document.getElementById('volver').addEventListener('click', function() {
+    document.querySelector('.registro-consulta').style.display = 'none';
+    document.querySelector('.menu-principal').style.display = 'block';
+});
+
+document.getElementById('limpiar').addEventListener('click', function() {
+    document.getElementById('formConsulta').reset();
+});
+
+document.getElementById('copiar').addEventListener('click', function() {
+    const respuestaTentativa = document.getElementById('respuestaTentativa').value;
+    document.getElementById('respuestaFinal').value = respuestaTentativa;
+});
