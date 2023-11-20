620b45045190b772a3aa0d6e45b0afb97c4da097
FeeeeeeD423 <150744506+FeeeeeeD423@users.noreply.github.com>
2023-11-20 16:42:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-document.getElementById('registroConsulta').addEventListener('click', function() {
-    document.querySelector('.menu-principal').style.display = 'none';
-    document.querySelector('.registro-consulta').style.display = 'block';
-});
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('registroConsulta').addEventListener('click', function() {
+        document.querySelector('.menu-principal').style.display = 'none';
+        document.querySelector('.registro-consulta').style.display = 'block';
+    });
 
-document.getElementById('volver').addEventListener('click', function() {
-    document.querySelector('.registro-consulta').style.display = 'none';
-    document.querySelector('.menu-principal').style.display = 'block';
-});
+    document.getElementById('volver').addEventListener('click', function() {
+        document.querySelector('.registro-consulta').style.display = 'none';
+        document.querySelector('.menu-principal').style.display = 'block';
+    });
 
-document.getElementById('limpiar').addEventListener('click', function() {
-    document.getElementById('formConsulta').reset();
-});
+    document.getElementById('ingresar').addEventListener('dblclick', function() {
+        document.getElementById('formConsulta').reset();
+    });
 
-document.getElementById('copiar').addEventListener('click', function() {
-    const respuestaTentativa = document.getElementById('respuestaTentativa').value;
-    document.getElementById('respuestaFinal').value = respuestaTentativa;
+    document.getElementById('copiar').addEventListener('click', function() {
+        const respuestaTentativa = document.getElementById('respuestaTentativa').value;
+        document.getElementById('respuestaFinal').value = respuestaTentativa;
+    });
 });
